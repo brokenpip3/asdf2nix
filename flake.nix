c662ddd8a54db968ae6aa10bdac660c346eeab6f
brokenpip3 <brokenpip3@gmail.com>
2025-12-31 15:15:41 +0100

    bump to 25.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Application packaged using poetry2nix";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
     poetry2nix = {
       url = "github:nix-community/poetry2nix";
